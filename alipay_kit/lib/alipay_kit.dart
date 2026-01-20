8f9d007f3a043b11902a6b923acd9c70b6e44ab3
ryanaltair <al_tair@live.com>
2026-01-20 12:19:41 +0800

    f

--- alipay_kit/lib/alipay_kit.dart
+++ alipay_kit/lib/alipay_kit.dart
@@ -3,6 +3,5 @@ library alipay_kit;
 
 export 'src/alipay_kit_platform_interface.dart';
 export 'src/constant.dart';
-export 'src/ios_auth_response.dart';
 export 'src/model/auth_result.dart';
 export 'src/model/resp.dart';
